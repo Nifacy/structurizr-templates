18e207bd67384e99e29be2d723673265ce9384f4
Alexey Grishin <95340036+Nifacy@users.noreply.github.com>
2025-02-27 22:44:06 +0300

    Revert "Move pattern information in class"

--- examples/pattern-plugins/src/main/java/com/patterns/params/ParseError.java
+++
@@ -1,8 +0,0 @@
-package com.patterns.params;
-
-public class ParseError extends Exception {
-
-    public ParseError(String message) {
-        super("Error raised during parse:\n" + message);
-    }
-}
