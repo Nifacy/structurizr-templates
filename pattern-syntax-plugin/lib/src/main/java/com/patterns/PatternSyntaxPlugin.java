cd5d8b5a6d471004bc3e3d246bcd3dc647f40ce3
Nifacy <mai.grishin.alexey@gmail.com>
2025-03-07 03:05:16 +0300

    pattern-syntax-plugin: rename plugin class

--- pattern-syntax-plugin/lib/src/main/java/com/patterns/PatternSyntaxPlugin.java
+++ pattern-syntax-plugin/lib/src/main/java/com/patterns/PatternSyntaxPlugin.java
@@ -4,7 +4,7 @@ import com.structurizr.dsl.StructurizrDslPlugin;
 import com.structurizr.dsl.StructurizrDslPluginContext;
 
 
-public class PatternPlugin implements StructurizrDslPlugin {
+public class PatternSyntaxPlugin implements StructurizrDslPlugin {
     @Override
     public void run(StructurizrDslPluginContext context) {
         PatternSyntaxEntity entity = new PatternSyntaxEntity();
